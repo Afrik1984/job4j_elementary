70cfe6074c428ff3614a044414fee45d78a526ed
Afrik1984 <familiKostin@yandex.ru>
2024-05-23 21:03:10 +0700

    6.4.3. Слово заканчивается на ... [#156317]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] postfix) {
+        boolean result = true;
+            for (int i = 0; i < postfix.length; i++) {
+                if (postfix[i] <= word[i]) {
+                    result = false;
+                    break;
+                }
+            }
+        return result;
+    }
+}
